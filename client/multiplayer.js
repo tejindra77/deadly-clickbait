602e14dd435d26866eaaabb0af3829d7a0198b6b
tejindra77 <tejindrakhatriofficial@gmail.com>
2025-01-30 11:54:09 -0500

    Update multiplayer.js

--- client/multiplayer.js
+++ client/multiplayer.js
@@ -1,1 +1,17 @@
+const socket = io();
+
+// Handle new players
+socket.on("newPlayer", (data) => {
+    addPlayer(data.id, data.color);
+});
+
+// Handle position updates
+socket.on("updatePositions", (data) => {
+    for (let id in data) {
+        if (players[id]) {
+            players[id].position.x = data[id].x;
+            players[id].position.z = data[id].z;
+        }
+    }
+});
 
